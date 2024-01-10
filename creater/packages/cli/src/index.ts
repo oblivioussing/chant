5802a84c0aadef13ff1408e9084f63293c89ae12
oblivioussing <oblivioussing@outlook.com>
2024-01-10 12:08:17 +0800

    cli

--- creater/packages/cli/src/index.ts
+++ creater/packages/cli/src/index.ts
@@ -5,6 +5,12 @@ import { select } from '@inquirer/prompts'
 init() // 初始化
 // 初始化
 async function init() {
-  const ret = await select({ message: 'Enter your name' })
+  const ret = await select({
+    message: '请选择生成的代码类型',
+    choices: [
+      { name: 'admin', value: 'admin' },
+      { name: 'nodejs', value: 'nodejs' }
+    ]
+  })
   console.log(ret)
 }
