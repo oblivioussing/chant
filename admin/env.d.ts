a9e992ca26a1cb23de37d59b1cc8b9bab9615795
oblivioussing <oblivioussing@outlook.com>
2024-03-26 09:35:06 +0800

    优化

--- admin/env.d.ts
+++ admin/env.d.ts
@@ -4,3 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_STAGE: 'dev' | 'test' | 'prod'
 }
+
+declare global {
+  interface Window {
+    __APP_VERSION__: string
+  }
+}
