9ea91c278508f9930d0036996cd9003cbfa21226
oblivioussing <oblivioussing@outlook.com>
2024-04-03 21:00:30 +0800

    优化

--- server/src/module/app.ts
+++ server/src/module/app.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { ScheduleModule } from '@nestjs/schedule'
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n'
+import * as path from 'path'
 import { AuthGuard } from '../guard/auth'
 import { RedisModule } from './redis/module'
 import sence from './scene'
@@ -9,6 +11,17 @@ import { ScheduleService } from '../schedule'
 
 @Module({
   imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '../i18n/'),
+        watch: true
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver
+      ]
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     RedisModule,
