3e946a4ce9de5520151f028c7df40e303a06cc83
oblivioussing <oblivioussing@outlook.com>
2025-06-19 17:21:26 +0800

    职位新增默认设置组织架构的角色

--- admin/packages/app-base/src/views/auth/position/components/MixForm.vue
+++ admin/packages/app-base/src/views/auth/position/components/MixForm.vue
@@ -17,7 +17,7 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { useFormer } from 'chant'
+import { shiki, useFormer } from 'chant'
 import type { FormProps, FormEmits } from 'chant/type'
 import { columns } from '../share'
 import RoleTreeSelect from '@app-base/views/auth/components/RoleTreeSelect.vue'
@@ -36,10 +36,20 @@ let state = reactive({
 former.created((status) => {
   // 获取详情
   status && getDetail()
+  // 获取组织详情
+  if (props.pageType === 'add') {
+    getOrgDetail()
+  }
 }, state)
 // 获取详情
 function getDetail() {
   former.getData('position/detail', state)
+}
+// 获取组织详情
+async function getOrgDetail() {
+  const id = state.selection.orgId
+  const { data } = await shiki.get('org/detail', { id })
+  state.form.roleIds = data?.roleIds
 }
 // 保存
 function onSave() {
